61c5f5e0d5e6bd900acb7681d769830533feb546
RamyMakram <RamyMakramEyd@outlook.com>
2019-12-15 18:52:40 +0200

    Ramy

--- web/Form1.Designer.cs
+++ web/Form1.Designer.cs
@@ -31,79 +31,246 @@ namespace web
         private void InitializeComponent()
         {
             this.web = new System.Windows.Forms.WebBrowser();
-            this.txturl = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.Start = new System.Windows.Forms.NumericUpDown();
+            this.End = new System.Windows.Forms.NumericUpDown();
+            this.SheetNum = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.LBLSheetPath = new System.Windows.Forms.Label();
+            this.LBLImgPath = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.LBLCount = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.Start)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.End)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SheetNum)).BeginInit();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // web
             // 
-            this.web.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.web.Location = new System.Drawing.Point(-3, 53);
+            this.web.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.web.Location = new System.Drawing.Point(0, 150);
             this.web.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.web.MinimumSize = new System.Drawing.Size(17, 16);
             this.web.Name = "web";
-            this.web.Size = new System.Drawing.Size(927, 554);
+            this.web.Size = new System.Drawing.Size(652, 453);
             this.web.TabIndex = 0;
             // 
-            // txturl
+            // button1
             // 
-            this.txturl.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txturl.Location = new System.Drawing.Point(286, 10);
-            this.txturl.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.txturl.Multiline = true;
-            this.txturl.Name = "txturl";
-            this.txturl.RightToLeft = System.Windows.Forms.RightToLeft.No;
-            this.txturl.Size = new System.Drawing.Size(540, 31);
-            this.txturl.TabIndex = 1;
+            this.button1.Location = new System.Drawing.Point(565, 53);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(29, 20);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Sheet";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(24, 71);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(94, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Image Folder";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Start
+            // 
+            this.Start.Location = new System.Drawing.Point(417, 23);
+            this.Start.Maximum = new decimal(new int[] {
+            99999999,
+            0,
+            0,
+            0});
+            this.Start.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(120, 20);
+            this.Start.TabIndex = 6;
+            this.Start.Value = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            // 
+            // End
+            // 
+            this.End.Location = new System.Drawing.Point(417, 56);
+            this.End.Maximum = new decimal(new int[] {
+            99999999,
+            0,
+            0,
+            0});
+            this.End.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.End.Name = "End";
+            this.End.Size = new System.Drawing.Size(120, 20);
+            this.End.TabIndex = 6;
+            this.End.Value = new decimal(new int[] {
+            6800,
+            0,
+            0,
+            0});
+            // 
+            // SheetNum
+            // 
+            this.SheetNum.Location = new System.Drawing.Point(417, 89);
+            this.SheetNum.Maximum = new decimal(new int[] {
+            99999999,
+            0,
+            0,
+            0});
+            this.SheetNum.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.SheetNum.Name = "SheetNum";
+            this.SheetNum.Size = new System.Drawing.Size(120, 20);
+            this.SheetNum.TabIndex = 6;
+            this.SheetNum.Value = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(196, 14);
+            this.label1.Location = new System.Drawing.Point(370, 28);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(58, 25);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "URL";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Start";
             // 
-            // button1
+            // label2
             // 
-            this.button1.Location = new System.Drawing.Point(88, 17);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 4;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(370, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "End";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(330, 95);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Sheet Number";
+            // 
+            // LBLSheetPath
+            // 
+            this.LBLSheetPath.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.LBLSheetPath.Location = new System.Drawing.Point(127, 45);
+            this.LBLSheetPath.Name = "LBLSheetPath";
+            this.LBLSheetPath.Size = new System.Drawing.Size(260, 13);
+            this.LBLSheetPath.TabIndex = 7;
+            this.LBLSheetPath.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // LBLImgPath
+            // 
+            this.LBLImgPath.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.LBLImgPath.Location = new System.Drawing.Point(83, 109);
+            this.LBLImgPath.Name = "LBLImgPath";
+            this.LBLImgPath.Size = new System.Drawing.Size(309, 13);
+            this.LBLImgPath.TabIndex = 7;
+            this.LBLImgPath.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.LBLCount);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.web);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.LBLSheetPath);
+            this.panel1.Controls.Add(this.Start);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.End);
+            this.panel1.Controls.Add(this.LBLImgPath);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.SheetNum);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.panel1.Size = new System.Drawing.Size(652, 603);
+            this.panel1.TabIndex = 10;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // LBLCount
+            // 
+            this.LBLCount.AutoSize = true;
+            this.LBLCount.Location = new System.Drawing.Point(588, 109);
+            this.LBLCount.Name = "LBLCount";
+            this.LBLCount.Size = new System.Drawing.Size(0, 13);
+            this.LBLCount.TabIndex = 10;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(925, 603);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.web);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.txturl);
+            this.ClientSize = new System.Drawing.Size(652, 603);
+            this.Controls.Add(this.panel1);
             this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
             this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.Text = "Form1";
+            this.RightToLeftLayout = true;
+            this.Text = "Home";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Start)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.End)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SheetNum)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.WebBrowser web;
-        private System.Windows.Forms.TextBox txturl;
-        private System.Windows.Forms.Label label1;
         private Button button1;
         private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private Button button2;
+        private Button button3;
+        private NumericUpDown Start;
+        private NumericUpDown End;
+        private NumericUpDown SheetNum;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label LBLSheetPath;
+        private Label LBLImgPath;
+        private Panel panel1;
+        private Label LBLCount;
     }
 }
 
--- web/Form1.cs
+++ web/Form1.cs
@@ -23,54 +23,120 @@ namespace web
         OleDbConnection con = new OleDbConnection(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=Database3.accdb");
         int url = 3;
         DataSet ds;
-        bool Enter = true;
+        private bool Enter = true;
         Microsoft.Office.Interop.Excel.Worksheet sh;
-        Thread Task;
+        string PathSheet = "";
+        string ImgFolder = "";
+        System.Timers.Timer t;
         public Form1()
         {
             InitializeComponent();
-            Microsoft.Office.Interop.Excel.Application ExcelApp = new Microsoft.Office.Interop.Excel.Application();
-            ExcelApp.Visible = true;
-            Microsoft.Office.Interop.Excel.Workbook wb = ExcelApp.Workbooks.Open(Directory.GetCurrentDirectory() + "\\Sheet6000.xlsx", Missing.Value, Missing.Value, Missing.Value, Missing.Value, Missing.Value, Missing.Value, Missing.Value, Missing.Value, Missing.Value, Missing.Value, Missing.Value, Missing.Value, Missing.Value, Missing.Value);
-            sh = (Microsoft.Office.Interop.Excel.Worksheet)wb.Sheets[2];
-            web.DocumentCompleted += new WebBrowserDocumentCompletedEventHandler(webBrowser1_DocumentCompleted);
-            System.Timers.Timer t = new System.Timers.Timer(3000);
-            t.Elapsed += new System.Timers.ElapsedEventHandler(Session_Start);
-            t.Start();
         }
         void Session_Start(object sender, System.Timers.ElapsedEventArgs e)
         {
-            Debug.WriteLine(url);
-            if (url == 3)
+            if (url == Start.Value)
             {
                 string ProName = sh.Cells[url, 2].Value.ToString();
                 string ss = "https://www.google.com/search?biw=1366&bih=663&tbm=isch&sxsrf=ACYBGNRsFK23-nOW5PQf_E_XfAHl3lGxrg%3A1576403238675&sa=1&ei=JgH2XYXMKNTygQb5loeYCA&q=" + Uri.EscapeDataString(ProName);
                 web.Navigate(ss);
                 url++;
             }
-            else if (!Enter && url <= 6428)
+            else if (!Enter && url <= End.Value)
             {
                 Enter = true;
                 string ProName = sh.Cells[url, 2].Value.ToString();
                 string ss = "https://www.google.com/search?biw=1366&bih=663&tbm=isch&sxsrf=ACYBGNRsFK23-nOW5PQf_E_XfAHl3lGxrg%3A1576403238675&sa=1&ei=JgH2XYXMKNTygQb5loeYCA&q=" + Uri.EscapeDataString(ProName);
                 web.Navigate(ss);
                 url++;
+                LBLCount.Text = url.ToString();
             }
+            else if (url > End.Value)
+            {
+                t.Stop();
+                button1.Enabled = true;
+                this.panel1.Enabled = true;
+                LBLCount.Text = url.ToString();
+            }
+            Debug.WriteLine(url);
         }
 
         private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
         {
-            HtmlElementCollection s = this.web.Document.GetElementsByTagName("img");
-            for (int i = 0; i < s.Count; i++)
+            try
             {
-                string imgUrl = s[i].GetAttribute("src");
-                if (imgUrl != "" && !imgUrl.StartsWith("https://www.google") && !imgUrl.Split(',')[0].Contains("svg"))
+                HtmlElementCollection s = this.web.Document.GetElementsByTagName("img");
+                for (int i = 0; i < s.Count; i++)
                 {
-                    string base64 = imgUrl.Split(',')[1];
-                    File.WriteAllBytes("D:\\Images\\" + sh.Cells[url-1, 1].Value.ToString() + ".png", Convert.FromBase64String(base64));
-                    Enter = false;
-                    break;
+                    string imgUrl = s[i].GetAttribute("src");
+                    if (imgUrl != "" && !imgUrl.StartsWith("https://www.google") && !imgUrl.Split(',')[0].Contains("svg"))
+                    {
+                        string base64 = imgUrl.Split(',')[1];
+                        File.WriteAllBytes(ImgFolder + "\\" + sh.Cells[url - 1, 1].Value.ToString() + ".png", Convert.FromBase64String(base64));
+                        Enter = false;
+                        break;
+                    }
                 }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("error in " + url.ToString());
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (PathSheet != "" && ImgFolder != "")
+            {
+                LBLCount.Text = url.ToString();
+                button1.Enabled = false;
+                this.panel1.Enabled = false;
+                url = (int)Start.Value;
+                Microsoft.Office.Interop.Excel.Application ExcelApp = new Microsoft.Office.Interop.Excel.Application();
+                ExcelApp.Visible = true;
+                Microsoft.Office.Interop.Excel.Workbook wb = ExcelApp.Workbooks.Open(PathSheet, Missing.Value, Missing.Value, Missing.Value, Missing.Value, Missing.Value, Missing.Value, Missing.Value, Missing.Value, Missing.Value, Missing.Value, Missing.Value, Missing.Value, Missing.Value, Missing.Value);
+                sh = (Microsoft.Office.Interop.Excel.Worksheet)wb.Sheets[SheetNum.Value];
+                web.DocumentCompleted += new WebBrowserDocumentCompletedEventHandler(webBrowser1_DocumentCompleted);
+                t = new System.Timers.Timer(3000);
+                t.Elapsed += new System.Timers.ElapsedEventHandler(Session_Start);
+                t.Start();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog
+            {
+                InitialDirectory = @"D:\",
+                Title = "Browse Exel Files",
+
+                CheckFileExists = true,
+                CheckPathExists = true,
+
+                DefaultExt = "xlxs",
+                Filter = "Excel Files (*.xlsx)|*.xlsx",
+                FilterIndex = 2,
+                RestoreDirectory = true,
+
+                ReadOnlyChecked = false,
+                ShowReadOnly = true
+            };
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                PathSheet = openFileDialog1.FileName;
+                LBLSheetPath.Text = PathSheet;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderDlg = new FolderBrowserDialog();
+            folderDlg.ShowNewFolderButton = true;
+            DialogResult result = folderDlg.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                ImgFolder = folderDlg.SelectedPath;
+                LBLImgPath.Text = ImgFolder;
             }
         }
     }
